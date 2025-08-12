2264eb6665fa1c0f689a2680ea43536bdb1743ca
GISLAINE DE CASSIA GAVIOLI <gislainegavioli.gg@gmail.com>
2025-08-12 18:07:08 +0000

    Criei a primeira carta super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,59 @@
 //Teste larissa
 
 int main() {
+    
+    char estado;
+    char codigo[5];
+    char cidade[15];
+    int populacao;
+    float area;
+    float pib;
+    int numero_pontos_turisticos;
+
+    printf("Carta 1\n");
+
+    printf("Digite a letra do Estado: ");
+    scanf(" %c" , &estado);
+
+    printf("Digite o Código da carta: ");
+    scanf("%s" , codigo);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s" , cidade);
+
+    printf("Digite a população: ");
+    scanf("%d" , &populacao);
+
+    printf("Digite a área em km²: ");
+    scanf("%f" , &area);
+
+    printf("Digite o PIB: ");
+    scanf("%f" , &pib);
+
+    printf("Digite o número de Pontos Turísticos: " , "\n");
+    scanf("%d" , &numero_pontos_turisticos);
+
+    printf("Carta 1\n");
+
+    printf("Estado:%c\n" , estado);
+    
+    printf("Código:%s\n" , codigo);
+
+    printf("Cidade:%s\n" , cidade);
+
+    printf("População:%d\n" , populacao);
+
+    printf("Área:%f\n" , area);
+
+    printf("PIB:%f\n" , pib);
+
+    printf("Número de Pontos Turísticos:%d\n" , numero_pontos_turisticos);
+
+
+
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
